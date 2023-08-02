447ae86080fab964319ffaab8cc43d35d67a46cb
lrmoore2023 <lrmoore2023@gmail.com>
2023-08-02 10:48:58 -0400

    Changes

--- js/index.js
+++ js/index.js
@@ -1,13 +1,18 @@
 $(document).ready(function() {
     const imageWrapper = $("#image-wrapper"); 
     let prevPercentage = 0;
+
+    $(window).on("click", function(){
+        console.log("Mouse clicked")
+    });
   
     $(window).mousedown(function(e) {
+        /*Store mouse position */
         imageWrapper.data("mouseDownAtPos", e.clientX);
-        console.log("clicked");
     });
 
     $(window).mouseup(function() {
+        /*Reset mouse down pos to 0 */
         imageWrapper.data("mouseDownAtPos", 0);
         prevPercentage = parseFloat(imageWrapper.data("percentage"));
       
@@ -19,13 +24,16 @@ $(document).ready(function() {
         const mouseDiff = parseFloat(imageWrapper.data("mouseDownAtPos") - e.clientX);
         const maxDiff = window.innerWidth / 2;
 
+        /*Calculate percentage based on existing diff + maxdiff */
         const percentage = (mouseDiff / maxDiff) * -100; 
         const nextPercentageInfinite = prevPercentage + percentage;
         const nextPercentage = Math.max(Math.min(nextPercentageInfinite, 0), -100);
 
+        /*Move images based on slider */
         imageWrapper.data("percentage", nextPercentage);
         imageWrapper.css("transform", `translate(${nextPercentage}%, -50%)`)
         
+        /*Slide/update image for parallax */
         $(".image", imageWrapper).each(function() {
             $(this).css("objectPosition", `${100 + nextPercentage}% center`);
         });   
